6fc5911d481555e9340c6469b3e4d257528150bf
syedakainat2222-glitch <syedakainat2222@gmail.com>
2026-07-22 10:59:20 +0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-console.log("POSTGRES_URL =", process.env.POSTGRES_URL);
+
 console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 const globalForPool = globalThis as unknown as {
@@ -10,8 +10,7 @@ const globalForPool = globalThis as unknown as {
 export const pool =
   globalForPool.pgPool ??
   new Pool({
-    connectionString:
-      process.env.POSTGRES_URL || process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 
     ssl: {
       rejectUnauthorized: false,
